4f3bb87efc59ff5035597af95e1dddc88346d7e0
Amine_Abbouti <amine.abbouti@hotmail.com>
2025-06-15 23:43:46 +0200

    No functional changes were made to the file."

--- app/Constants/HttpStatus.php
+++ app/Constants/HttpStatus.php
@@ -5,22 +5,13 @@ namespace App\Constants;
 class HttpStatus
 {
     public const HTTP_OK = 200;
-
     public const HTTP_CREATED = 201;
-
     public const HTTP_NO_CONTENT = 204;
-
     public const HTTP_BAD_REQUEST = 400;
-
     public const HTTP_UNAUTHORIZED = 401;
-
     public const HTTP_FORBIDDEN = 403;
-
     public const HTTP_NOT_FOUND = 404;
-
     public const HTTP_CONFLICT = 409;
-
     public const HTTP_UNPROCESSABLE_ENTITY = 422;
-
     public const HTTP_SERVER_ERROR = 500;
 }
